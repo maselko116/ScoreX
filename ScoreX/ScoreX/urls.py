6587552447dab050fe9d7c18eb95917a48d7f405
maselko116 <kiku116m@gmail.com>
2023-05-22 17:38:56 +0200

    SECOND WERSION WITH HTML, DB AND CSS

--- ScoreX/ScoreX/urls.py
+++ ScoreX/ScoreX/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from base.views import index
+
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
 
     
---
+++ ScoreX/item/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+   #imagen = models.ImageField(upload_to='item_images', blank=True, null = True) 
+    def __str__(self):
+        return self.name
