abdb1c3aa196c0846e13b7a36e178293798fcd48
maselko116 <kiku116m@gmail.com>
2023-05-24 14:03:13 +0200

    wyswietlanie items z bazy danych

--- ScoreX/base/views.py
+++ ScoreX/base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
 # Create your views here.
 def index(request):
-    return render(request, 'base/index.html')
+    categories = Category.objects.all()
+    items = Item.objects.all()
+
+    return render(request, 'base/index.html', {
+        'categories': categories,
+        'items': items,
+    })
 def style(request):
     return render(request, 'frame.html')
--- ScoreX/item/models.py
+++ ScoreX/item/models.py
@@ -15,7 +15,7 @@ class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
     price = models.FloatField()
-   #imagen = models.ImageField(upload_to='item_images', blank=True, null = True) 
     def __str__(self):
         return self.name
